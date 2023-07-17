841afb88e04d29ba8502688cecad951f6f58911c
Ahmad Jarara <git@ajarara.io>
2023-07-16 23:47:40 -0400

    Avoid tui-force-update in tui-timer
    Pre-diff works fine, but this was the first place I looked for 'how do
    I have external processes communicate updates to tui', and the pattern
    of 'update and poll' instead of 'update with new result' only works
    for elisp code that runs quick.

--- components/tui-timer.el
+++ components/tui-timer.el
@@ -1,4 +1,4 @@
-;;; tui-demo-timer.el --- A basic timer       -*- lexical-binding: t; -*-
+;;; tui-timer.el --- A basic timer       -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 ;; 
@@ -8,25 +8,41 @@
 (require 'tui-defun)
 (require 'tui-util)
 
-(tui-defun-2 tui-demo-timer (&state (start-time (current-time))
-                                    (timer (tui-run-with-timer
-                                            component 1 1 t
-                                            (lambda ()
-                                              (tui-force-update component)))))
-  "A basic timer"
-  (let* ((start-time-seconds (truncate (time-to-seconds start-time)))
-         (current-time-seconds (truncate (time-to-seconds)))
-         (elapsed-seconds (- current-time-seconds start-time-seconds))
-         (days (truncate (/ elapsed-seconds 86400)))
-         (hours (truncate (/ (% elapsed-seconds 86400) 3600)))
-         (minutes (truncate (/ (% elapsed-seconds 3600) 60)))
-         (seconds (truncate (% elapsed-seconds 60))))
-    (tui-span
-     (when (> days 0)
-       (format "%d days " days))
-     (when (> hours 0)
-       (format "%02d" hours))
-     (format "%02d:%02d" minutes seconds))))
+(tui-define-component tui-timer
+  :component-did-mount
+  (lambda (component)
+    (cl-labels ((tick ()
+                      (tui-run-with-timer component
+                                          1
+                                          nil
+                                          nil
+                                          (lambda ()
+                                            (let ((next-state (copy-sequence (tui-get-state component))))
+                                              (plist-put next-state 'current-time-seconds (truncate (time-to-seconds)))
+                                              (tui-set-state component next-state))
+                                            (tick)))))
+      (tick)))
+  :get-initial-state
+  (lambda (_)
+    (let ((start-time (current-time)))
+      (list 'start-time start-time
+            'current-time-seconds (truncate (time-to-seconds start-time)))))
+  :render
+  (lambda (component)
+    (let* ((state (tui-get-state component))
+           (start-time-seconds (truncate (time-to-seconds (plist-get state 'start-time))))
+           (current-time-seconds (plist-get state 'current-time-seconds))
+           (elapsed-seconds (- current-time-seconds start-time-seconds))
+           (days (truncate (/ elapsed-seconds 86400)))
+           (hours (truncate (/ (% elapsed-seconds 86400) 3600)))
+           (minutes (truncate (/ (% elapsed-seconds 3600) 60)))
+           (seconds (truncate (% elapsed-seconds 60))))
+      (tui-span
+       (when (> days 0)
+         (format "%d days " days))
+       (when (> hours 0)
+         (format "%02d" hours))
+       (format "%02d:%02d" minutes seconds)))))
 
 (provide 'tui-timer)
 ;;; tui-demo-timer.el ends here
